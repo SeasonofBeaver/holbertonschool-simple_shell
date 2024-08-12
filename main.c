429b9e889c953ce456c5cea0a020163683c79823
SeasonofBeaver <171557982+SeasonofBeaver@users.noreply.github.com>
2024-08-12 11:18:33 +0200

    added main and changed header

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+int main(void)
+{
+	while(1)
+	{
+		check if user input something
+		see if it makes sence
+		if not 
+			continue;
+		use what they input as command
+		execute command
+		return whatever the command did
+		if user exits end program
+			exit(97);
+	}
+	return (0);
+}
