7e02e0281ef68ff640cb05f01381350f7bcca0b7
Tissem-Ben <t.bendahma@gmail.com>
2024-08-13 15:09:23 +0200

    Update main.c with new lines

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include "shell.h"
 
+void printPromt(const char *username, const char *currDir);
+int _exec(char **args, char *command);
+
 int main(void)
 {
 	size_t maxLength = 4096;
@@ -14,13 +17,24 @@ int main(void)
 	while (1)
 	{
 		printPromt(username, currDir);
+
 		getlineLength = getline(&input, &maxLength, stdin);
+		if (getlinelength == -1)
+		{
+			perror("getline");
+			free(currDir);
+			free(username);
+			free(input);
+			exit(EXIT_FAILURE);
+		}
+
 		command = input;
 		while (*command == ' ' || *command == '\t')
 			command++;
 		if (strcmp(command, "\n") == 0)
 			continue;
 		command[strlen(command) - 1] = '\0';
+		args = parseCommand(command);
 		if (args[0] == NULL)
 		{
 			fprintf(stderr, "./hsh: 1: %s: not found\n", strtok(command, " "));
@@ -33,5 +47,10 @@ int main(void)
 		}
 		free(args);
 	}
+
+	free(currDir);
+	free(username);
+	free(input);
+
 	return (0);
 }
