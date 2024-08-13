6150ecf0871ad3dd51d1095ce55c7342c34b8658
Tissem-Ben <t.bendahma@gmail.com>
2024-08-13 15:42:12 +0200

    Add getData.c with necessary corrections

---
+++ getData.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <string.h>
+
+char *getCurrentWorkingDirectory(void)
+{
+	char *cwd = malloc(PATH_MAX);
+	if (cwd == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	if (getcwd(cwd, PATH_MAX) == NULL)
+	{
+		perror("getcwd");
+		free(cwd);
+		exit(EXIT_FAILURE);
+	}
+	return cwd
+}
+char *getUsername(void)
+{
+	struct passwd *pw = getpwuid(geteuid());
+	if (pw == NULL)
+	{
+		perror("getpwuid");
+		exit(EXIT_FAILURE);
+	}
+	return strdup(pw->pw_name);
+}
